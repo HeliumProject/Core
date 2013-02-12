084ab42939622ce705cede3a4d81e300fe1b494d
Zach Brockway <neverender@gmail.com>
2013-02-11 18:35:32 -0800

    Remove PopulateComposite from Color4

--- Color.cpp
+++ Color.cpp
@@ -1,19 +1,10 @@
 #include "MathPch.h"
 #include "Math/Color3.h"
 #include "Math/Color4.h"
-#include "Reflect/Data/DataDeduction.h"
 
-REFLECT_DEFINE_BASE_STRUCTURE( Helium::Color4 );
+using namespace Helium;
 
-void Helium::Color4::PopulateComposite( Reflect::Composite& comp )
-{
-	comp.AddField( &Color4::r,       TXT( "r" ) );
-	comp.AddField( &Color4::g,       TXT( "g" ) );
-	comp.AddField( &Color4::b,       TXT( "b" ) );
-	comp.AddField( &Color4::a,       TXT( "a" ) );
-}
-
-Helium::Color3& Helium::Color3::operator=( const Helium::Color4& v )
+Color3& Color3::operator=( const Color4& v )
 {
     r = v.r;
     g = v.g;
@@ -21,7 +12,7 @@ Helium::Color3& Helium::Color3::operator=( const Helium::Color4& v )
     return *this;
 }
 
-Helium::Color3::operator Helium::Color4()
+Color3::operator Color4()
 {
-    return Helium::Color4( r, g, b, 255 );
+    return Color4( r, g, b, 255 );
 }
