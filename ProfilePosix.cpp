f3763568ea1e09c156acf6b7badd6b3f3ab6a47f
Geoff Evans <geoff@flummoxed.org>
2013-08-08 15:31:43 -0700

    add cpu clock check to linux

--- ProfilePosix.cpp
+++ ProfilePosix.cpp
@@ -3,6 +3,8 @@
 
 #include <pthread.h>
 #include <assert.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 void Helium::TraceFile::Open(const char* file)
 {
@@ -24,16 +26,63 @@ const char* Helium::TraceFile::GetFilePath()
     return NULL;
 }
 
+#if HELIUM_CC_GCC || HELIUM_CC_CLANG
+
+#if defined(__i386__)
+
+static HELIUM_FORCEINLINE uint64_t rdtsc(void)
+{
+    uint64_t int x;
+    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+    return x;
+}
+#elif defined(__x86_64__)
+
+static HELIUM_FORCEINLINE uint64_t rdtsc(void)
+{
+    unsigned hi, lo;
+    __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+    return ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
+}
+
+#endif
+
+#endif
+
 uint64_t Helium::TimerGetClock()
 {
-    uint64_t time = 0;
-    HELIUM_BREAK();
+    uint64_t time = rdtsc();
     return time;
+}
+
+inline double GetClockSpeed()
+{
+    static double clockSpeed = 0.0;
+    if (clockSpeed != 0.0)
+    {
+        return clockSpeed;
+    }
+
+#if HELIUM_OS_LINUX
+    int h = open( "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq", O_RDONLY );
+    if ( HELIUM_VERIFY( h >= 0 ) )
+    {
+        char buf[ 256 ];
+        size_t bytesRead = read( h, buf, sizeof( buf ) );
+        sscanf( buf, "%lf", &clockSpeed );
+        clockSpeed *= 1000.f;
+        close( h );
+    }
+#else
+    HELIUM_BREAK();
+#endif
+
+    return clockSpeed;
 }
 
 float Helium::CyclesToMillis(uint64_t cycles)
 {
-    return (float64_t)cycles * (float64_t)(1000.0 / 79800000ULL);
+    return (float64_t)cycles * (float64_t)(1000.0 / GetClockSpeed());
 }
 
 float Helium::TimeTaken(uint64_t start_time)
