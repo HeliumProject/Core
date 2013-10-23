8f1ab462de2843ed859e6ba7a60207db731776bc
Geoff Evans <geoff@flummoxed.org>
2013-10-23 11:47:55 -0700

    switch from macros to typedefs for tracefile handle

--- Profile.cpp
+++ Profile.cpp
@@ -5,6 +5,12 @@ using namespace Helium;
 
 #pragma TODO("Move this class into Timer.h/cpp when Timer subsystems merge")
 
+#if HELIUM_OS_WIN
+const TraceFile::Handle TraceFile::InvalidHandleValue = NULL;
+#else
+const TraceFile::Handle TraceFile::InvalidHandleValue = -1;
+#endif
+
 void SimpleTimer::Reset()
 {
     m_StartTime = Helium::TimerGetClock();
--- Profile.h
+++ Profile.h
@@ -5,63 +5,56 @@
 #include "Platform/Types.h"
 #include "Platform/Utility.h"
 
-#ifdef WIN32
-
-struct _iobuf;
-typedef struct _iobuf FILE;
-#define FILE_HANDLE FILE*
-#define INVALID_FILE_HANDLE NULL
-
-#else
-
-#define FILE_HANDLE int
-#define INVALID_FILE_HANDLE -1
-
-#endif
-
 namespace Helium
 {
-    class HELIUM_PLATFORM_API SimpleTimer : NonCopyable
-    {
-    public:
-        SimpleTimer()
-        {
-            Reset();
-        }
+	class HELIUM_PLATFORM_API SimpleTimer : NonCopyable
+	{
+	public:
+		SimpleTimer()
+		{
+			Reset();
+		}
 
-        // reset timer (for re-use)
-        void Reset();
+		// reset timer (for re-use)
+		void Reset();
 
-        // get elapsed time in millis
-        float Elapsed();
+		// get elapsed time in millis
+		float Elapsed();
 
-    private:
-        uint64_t m_StartTime;
-    };
+	private:
+		uint64_t m_StartTime;
+	};
 
-    class HELIUM_PLATFORM_API TraceFile
-    {
-    private:
-        FILE_HANDLE m_FileHandle;
+	class HELIUM_PLATFORM_API TraceFile
+	{
+	private:
+#ifdef HELIUM_OS_WIN
+		typedef FILE* Handle;
+#else
+		typedef int Handle;
+#endif
+		const static Handle InvalidHandleValue;
 
-    public:
-        TraceFile()
-            : m_FileHandle (INVALID_FILE_HANDLE)
-        {
+		Handle m_FileHandle;
 
-        }
+	public:
+		TraceFile()
+			: m_FileHandle (InvalidHandleValue)
+		{
 
-        void Open(const char* file);
-        void Close();
-        void Write(const char* data, int size);
+		}
 
-        static const char* GetFilePath();
-    };
+		void Open(const char* file);
+		void Close();
+		void Write(const char* data, int size);
 
-    HELIUM_PLATFORM_API uint64_t TimerGetClock();
-    HELIUM_PLATFORM_API float CyclesToMillis(uint64_t cycles);
-    HELIUM_PLATFORM_API float TimeTaken(uint64_t start_time);
-    HELIUM_PLATFORM_API void ReportTime(const char* segment, uint64_t start_time, double& total_millis);
+		static const char* GetFilePath();
+	};
 
-    HELIUM_PLATFORM_API uint64_t GetTotalMemory();
+	HELIUM_PLATFORM_API uint64_t TimerGetClock();
+	HELIUM_PLATFORM_API float CyclesToMillis(uint64_t cycles);
+	HELIUM_PLATFORM_API float TimeTaken(uint64_t start_time);
+	HELIUM_PLATFORM_API void ReportTime(const char* segment, uint64_t start_time, double& total_millis);
+
+	HELIUM_PLATFORM_API uint64_t GetTotalMemory();
 }
