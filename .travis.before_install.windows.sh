230370de61c33155f30f9f69a953825091a4b53e
Geoff Evans <geoff@flummoxed.org>
2018-12-28 12:44:34 -0800

    probe vswhere...

--- .travis.before_install.windows.sh
+++ .travis.before_install.windows.sh
@@ -2,4 +2,4 @@
 
 echo before_install...
 
-"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -all
+"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -products 'Microsoft.VisualStudio.Product.BuildTools'
