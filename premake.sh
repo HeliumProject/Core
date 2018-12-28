81ba2aa1f11955fc88858f3578da9e4764e97d0d
Geoff Evans <geoff@flummoxed.org>
2018-12-27 16:12:25 -0800

    Add git-bash support for windows to premake bootstrap

--- premake.sh
+++ premake.sh
@@ -1,19 +1,21 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ `uname` == "Darwin" ]; then
-	OS=osx
+if [ ! -f $DIR/Dependencies/premake/bin/release/premake5 ]; then
+
+	pushd $DIR/Dependencies/premake > /dev/null
+
+	if [ `uname` == "Darwin" ]; then
+		make -f Bootstrap.mak osx
+	elif [ `uname` == "Linux" ]; then
+		make -f Bootstrap.mak linux
+	else
+		VSINSTALL=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -property installationPath`
+		VSINSTALL_BASH=`cygpath -a "$VSINSTALL"`
+		cmd //c "$VSINSTALL_BASH/VC/Auxiliary/Build/vcvars64.bat" \&\& nmake -f Bootstrap.mak windows
+	fi
+
+	popd > /dev/null
+
 fi
-
-if [ `uname` == "Linux" ]; then
-	OS=linux
-fi
-
-pushd $DIR/Dependencies/premake > /dev/null
-
-if [ ! -f bin/release/premake5 ]; then
-	make -f Bootstrap.mak $OS
-fi
-
-popd > /dev/null
 
 $DIR/Dependencies/premake/bin/release/premake5 "$@"
