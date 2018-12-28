aea3862b31b235440384a987861afb1b347fa0ef
Geoff Evans <geoff@flummoxed.org>
2018-12-28 13:11:27 -0800

    add native desktop workload

--- .travis.before_install.windows.sh
+++ .travis.before_install.windows.sh
@@ -2,6 +2,6 @@
 
 echo before_install...
 
-choco install visualstudio2017community
+choco install visualstudio2017community visualstudio2017-workload-nativedesktop
 
 find "/c/Program Files (x86)/Microsoft Visual Studio/"
