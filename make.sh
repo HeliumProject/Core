185164bb4aa95d3ade10a200eb94234262386a19
Geoff Evans <geoff@flummoxed.org>
2018-12-28 22:39:30 -0800

    add some error detection to make.sh

--- make.sh
+++ make.sh
@@ -3,14 +3,27 @@
 pushd Build
 
 if [ `uname` == "Darwin" ]; then
+
 	JOBS=`sysctl -n hw.ncpu`
 	echo Building with $JOBS jobs
 	make -j$JOBS config=$1
+
+	if [ "$?" -ne "0" ]; then
+		exit 1
+	fi
+
 elif [ `uname` == "Linux" ]; then
+
 	JOBS=`nproc`
 	echo Building with $JOBS jobs
 	make -j$JOBS config=$1
+
+	if [ "$?" -ne "0" ]; then
+		exit 1
+	fi
+
 else
+
 	VSINSTALL=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -property installationPath`
 	if [ -z "$VSINSTALL" ]; then
 		VSINSTALL=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -products 'Microsoft.VisualStudio.Product.BuildTools' -property installationPath`
@@ -19,6 +32,11 @@ else
 	VSINSTALL_BASH=`cygpath -a "$VSINSTALL"`
 	SOLUTION=`cat solution.txt`
 	cmd //c "$VSINSTALL_BASH/VC/Auxiliary/Build/vcvars64.bat" \&\& msbuild $SOLUTION.sln //p:Configuration=$1 //verbosity:minimal
+
+	if [ "$?" -ne "0" ]; then
+		exit 1
+	fi
+
 fi
 
 popd
