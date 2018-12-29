cb246412240b6e05a7bbb1d73f73666fdb2910e0
Geoff Evans <geoff@flummoxed.org>
2018-12-28 21:25:21 -0800

    Attempt boostrap of premake using vs build tools only

--- .travis.before_install.windows.sh
+++ .travis.before_install.windows.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 echo before_install...
-
-choco install visualstudio2017community visualstudio2017-workload-nativedesktop
--- premake.sh
+++ premake.sh
@@ -25,14 +25,16 @@ if [ ! -f $DIR/Dependencies/premake/bin/release/premake5 ]; then
 	else
 
 		VSINSTALL=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -property installationPath`
+		VSVERSION=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -property catalog_productLineVersion`
+
 		if [ -z "$VSINSTALL" ]; then
 			VSINSTALL=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -products 'Microsoft.VisualStudio.Product.BuildTools' -property installationPath`
+			VSVERSION=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -products 'Microsoft.VisualStudio.Product.BuildTools' -property catalog_productLineVersion`
 		fi
-		echo $VSINSTALL
-		VSINSTALL_BASH=`cygpath -a "$VSINSTALL"`
-		echo $VSINSTALL_BASH
+
 		echo Calling premake bootstrap...
-		cmd //c "$VSINSTALL_BASH/VC/Auxiliary/Build/vcvars64.bat" \&\& nmake -f Bootstrap.mak windows
+		VSINSTALL_BASH=`cygpath -a "$VSINSTALL"`
+		cmd //c "$VSINSTALL_BASH/VC/Auxiliary/Build/vcvars64.bat" \&\& nmake -f Bootstrap.mak MSDEV=vs$VSVERSION windows-msbuild
 		if [ "$?" -ne "0" ]; then
 			exit 1
 		fi
