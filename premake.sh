77ad60bc6defc81890daff62b5a692d32e5a00c2
Geoff Evans <geoff@flummoxed.org>
2018-12-28 16:41:55 -0800

    shore up error handling

--- premake.sh
+++ premake.sh
@@ -7,10 +7,23 @@ if [ ! -f $DIR/Dependencies/premake/bin/release/premake5 ]; then
 	pushd $DIR/Dependencies/premake > /dev/null
 
 	if [ `uname` == "Darwin" ]; then
+
+		echo Calling premake bootstrap...
 		make -f Bootstrap.mak osx
+		if [ "$?" -ne "0" ]; then
+			exit 1
+		fi
+
 	elif [ `uname` == "Linux" ]; then
+
+		echo Calling premake bootstrap...
 		make -f Bootstrap.mak linux
+		if [ "$?" -ne "0" ]; then
+			exit 1
+		fi
+
 	else
+
 		VSINSTALL=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -property installationPath`
 		if [ -z "$VSINSTALL" ]; then
 			VSINSTALL=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -products 'Microsoft.VisualStudio.Product.BuildTools' -property installationPath`
@@ -20,6 +33,10 @@ if [ ! -f $DIR/Dependencies/premake/bin/release/premake5 ]; then
 		echo $VSINSTALL_BASH
 		echo Calling premake bootstrap...
 		cmd //c "$VSINSTALL_BASH/VC/Auxiliary/Build/vcvars64.bat" \&\& nmake -f Bootstrap.mak windows
+		if [ "$?" -ne "0" ]; then
+			exit 1
+		fi
+
 	fi
 
 	popd > /dev/null
