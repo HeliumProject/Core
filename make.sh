d2be152a447f555048b173c820fd69e0f0a74ad4
Geoff Evans <geoff@flummoxed.org>
2018-12-28 21:42:26 -0800

    make.sh support for vs build tools

--- make.sh
+++ make.sh
@@ -12,6 +12,10 @@ elif [ `uname` == "Linux" ]; then
 	make -j$JOBS config=$1
 else
 	VSINSTALL=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -property installationPath`
+	if [ -z "$VSINSTALL" ]; then
+		VSINSTALL=`"/c/Program Files (x86)/Microsoft Visual Studio/Installer/vswhere.exe" -products 'Microsoft.VisualStudio.Product.BuildTools' -property installationPath`
+	fi
+
 	VSINSTALL_BASH=`cygpath -a "$VSINSTALL"`
 	SOLUTION=`cat solution.txt`
 	cmd //c "$VSINSTALL_BASH/VC/Auxiliary/Build/vcvars64.bat" \&\& msbuild $SOLUTION.sln //p:Configuration=$1 //verbosity:minimal
