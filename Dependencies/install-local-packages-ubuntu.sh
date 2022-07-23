89c2475de988348dbd0c69dba7cd3ec1f13a63ac
Geoff Evans <geoff@flummoxed.org>
2022-07-23 16:28:03 -0400

    Use http transport because GH actions workers can have ssl errors sometimes

--- Dependencies/install-local-packages-ubuntu.sh
+++ Dependencies/install-local-packages-ubuntu.sh
@@ -2,7 +2,7 @@
 
 # local packages
 rm -rf mongodb/
-curl -L -omongodb-linux-x86_64-ubuntu2004-5.0.9.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.9.tgz
+curl -L -omongodb-linux-x86_64-ubuntu2004-5.0.9.tgz http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-5.0.9.tgz
 tar -xvf mongodb-linux-x86_64-ubuntu2004-5.0.9.tgz
 rm mongodb-linux-x86_64-ubuntu2004-5.0.9.tgz
 mv mongodb-linux-x86_64-ubuntu2004-5.0.9 mongodb
